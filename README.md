120aa903e5a187a2df06a1d69c10883c1dd672f3
java2147483647 <108693684+java2147483647@users.noreply.github.com>
2023-10-26 11:54:37 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# drake-quotes
+Simple website that displays a random Drake quote each time the page is refreshed. Style inspired by the cover of Titles Ruin Everything.
---
+++ script.js
@@ -0,0 +1,31 @@
+const quotesTxt = 
+`It's the pretty boys versus the petty boys
+Houston they get me though, European my vehicle
+They say love's like a BBL, you won't know if it's real until you feel one
+Man got flown like private jets for way way less
+Still steppin' like Omega Psi Phi for mine
+At the table praying banker shows a natural 9, It never happens half the time
+Had to pull my niggas out the mud like I'm training marines
+I feel like lately it went from top 5 to remaining 5
+Cris' bottle sender, check-picker-upper
+The part I love most is they need me more than they hate me
+As luck would have it, I've settled into my role as the good guy
+Live a little, 'cause niggas die a lot
+Please excuse my table manners, I was making room for the table dancers
+You need to act your age and not your girl's age
+I'll probably self-destruct if I ever lose but I never do`
+
+const quotes = quotesTxt.split("\n");
+outputRandomQuote(quotes);
+
+function outputRandomQuote(quotesArray) {
+    var numberOfQuotes = (quotesArray.length);
+    var x = getRandomIntInclusive(0, numberOfQuotes - 1); 
+    document.getElementById("quote").innerHTML = quotesArray[x].toUpperCase();
+}
+
+function getRandomIntInclusive(min, max) { // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
