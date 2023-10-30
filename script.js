f360d5bae12f5683ef854b07e008b2acf7984cd2
java2147483647 <108693684+java2147483647@users.noreply.github.com>
2023-10-29 19:04:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,20 @@ Live a little, 'cause niggas die a lot
 Please excuse my table manners, I was making room for the table dancers
 You need to act your age and not your girl's age
 I'm a bill printer, I'm a grave digger
-I'll probably self-destruct if I ever lose but I never do`
+I'll probably self-destruct if I ever lose but I never do
+That was back in the days, Acura days
+Is it the fall? Time for me to revisit the past
+She says "You're such a dog," I say "You're such a bone"
+Looking for the right way to do the wrong things
+I'm honest, I make mistakes, I'd be the second to admit it
+I just set the bar, niggas fall under it like a limbo
+Your mama used to live at the church on Sunday, you just go to LIV after church on Sunday
+I've been stackin' up like I'm fundraising
+It's supposed to be a lot of hard workin' goin' on, but who the fuck can focus with this twerkin' goin' on?
+Drinkin', smokin', fuckin', plottin, schemin', plottin', schemin', gettin' money
+Your lack of effort got me rappin' different
+Yours truly, the boy
+Cashmere knits for the nighttime boat rides`
 
 const quotes = quotesTxt.split("\n");
 outputRandomQuote(quotes);
